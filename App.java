530ac5ebe85273e3b7a933141cbc88cabf6669d2
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-17 11:16:59 +0300

    have created an app witch will place car object in a parking place at the Mall

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+
+    
+       // Let's create a parking lot
+       Parking mallParking = new Parking(6);
+       //Let's create a car
+       Car sUVCar = new Car("Mercedes");
+
+       System.out.println(mallParking);
+
+    }
+}
